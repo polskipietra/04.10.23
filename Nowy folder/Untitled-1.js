d178bfcade57ae4ea4402a20b0af2447007ca88a
polskipietra <109820324+polskipietra@users.noreply.github.com>
2023-10-04 11:15:56 +0200

    Add files via upload

---
+++ Nowy folder/Untitled-1.js
@@ -0,0 +1,57 @@
+//zad.1
+// alert("Hello world!")
+
+//zad.2
+// const input = document.querySelector(".input");
+// const button = document.querySelector(".button");
+// function wyswietl() {
+// 	alert(input.value);
+// }
+// button.addEventListener("click", wyswietl);
+
+//zad.3
+// if (confirm("podejmij decyzję !")) {
+// 	alert("Wybrano opcję: OK");
+// } else {
+// 	alert("Wybrano opcję: Anuluj");
+// }
+
+//zad.4
+// let imie = prompt("Podaj imie", "Uszatek");
+// document.write("Jesteś " + imie);
+
+//zad.5
+// console.log("Hello Word");
+
+// // zad.6
+// let mojeimie = 'Kuba';
+// console.log(mojeimie);
+
+// // zad.7
+// let a = 3;
+// let b = 4;
+// let c = a + b;
+// alert(c);
+
+// zad.8
+// let temp = 36.6;
+
+// zad.9
+// let x = 99;
+// let zmienna = "spraerdzam typeofa";
+// console.log(typeof x);
+// console.log(typeof zmienna);
+
+// zad.10
+// let x = 99;
+// let zmienna = "spraerdzam typeofa";
+// alert(`${zmienna} liczby ${x}, jest to ${typeof x}`)
+
+// zad.11
+// const a = "Cześć ";
+// const b = "mam ";
+// const c = "na imie ";
+// const d = "Kuba, ";
+// const e = "mam 16 lat";
+// console.log(a, b, c, d, e);
+// zad.12
